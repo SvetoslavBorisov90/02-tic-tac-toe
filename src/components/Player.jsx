9fb50e32e474228f0dd454dc8faa0cae1190653d
SvetoslavBorisov90 <svetoslavborisov90@gmail.com>
2024-08-22 21:18:00 +0300

    cleared some not used comments

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -21,13 +21,11 @@ export default function Player({
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-  // let btnCaption = "Edit";
 
   if (isEditing) {
     editablePlayerName = (
       <input type="text" required value={playerName} onChange={handleChange} />
     );
-    // btnCaption = "Save";
   }
 
   return (
